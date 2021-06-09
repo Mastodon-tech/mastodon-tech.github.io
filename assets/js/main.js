3334db58167207d11c34cac065056809caa4f137
Nafiz Pervez <nafizpervez@gmail.com>
2021-06-09 12:40:20 +0600

    third iteration

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,4 +62,13 @@
     });
   });
 
+
+  //preloader
+  $('.preloader').delay(3000).fadeOut('slow');
+    setTimeout(function() {
+    //After 3s, the no-scroll class of the body will be removed
+  $('body').removeClass('no-scroll');
+    }, 3000); //Here you can change preloader time
+
+
 })(jQuery);
